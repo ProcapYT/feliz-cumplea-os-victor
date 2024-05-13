edf637cb92493b4b14dcba2c99d7d705869b2d77
ProcapYT <samuelpedrera7@gmail.com>
2024-05-13 16:48:28 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const giftButton = document.querySelector(".gift-button");
+
+giftButton.addEventListener("click", () => {
+    open("https://paradores.es/es/caja-regalo-una-noche-con-desayuno-para-dos", "_blank");
+});
